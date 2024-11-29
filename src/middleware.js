986cb2cd4a5b8d0bef37380a076aff478917961c
saif01010 <saiful.12006518@lpu.in>
2024-11-29 21:42:11 +0530

    Update middleware configuration to include new routes for search and saveList

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import {  NextResponse } from 'next/server';
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/sign-in', '/sign-up', '/home/:path*','/brewery/:path*'],
+  matcher: ['/sign-in', '/sign-up', '/search','/saveList'],
 };
 
 export async function middleware(request) {
